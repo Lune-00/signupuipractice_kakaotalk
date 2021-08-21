595300f70c31d4d6a2ba24c7321a6fcae1d40ec9
Lune <lune9402@naver.com>
2021-08-21 22:15:58 +0900

    체크박스 내부색상 아이콘 색상 미적용 버튼 테두리 텍스트 밑줄_링크 이동
    _해결해야 함

--- app/src/main/java/com/example/ui_kakaotalk/MainActivity.kt
+++ app/src/main/java/com/example/ui_kakaotalk/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.ui_kakaotalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import androidx.core.text.buildSpannedString
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
